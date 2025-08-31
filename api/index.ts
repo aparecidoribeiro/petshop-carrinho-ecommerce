87b396b91b9332ae60c1f7af907a7281a8211a00
Aparecido Ribeiro <aparecidoribeiro.dev@gmail.com>
2025-08-30 23:06:28 -0300

    modificações

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
+import jsonServer from "json-server";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json")
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
